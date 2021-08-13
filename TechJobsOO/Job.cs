05dfd7616df4e19b19e4887e45de7caf75857284
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-08-13 17:12:35 -0500

    Add two constructor to Job class. Generate and Equal() and GetHashCode() method.
    First constructor initializes the id field and take no parameters. Second constructor takes 5 parameters and assign values to name, employerName, employerLocation, jobType, and jobCoreCompetency

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -14,6 +14,34 @@ namespace TechJobsOO
 
         // TODO: Add the two necessary constructors.
 
+        //constructor 1 of Job class: initialize id field with a unqiue value. Takes no parameter.
+        public Job()
+        {
+            Id = 00001;
+        }
+
+        //constructor 2: Takes 5 parameter and assign values to them //Use constructor chaining to call first constructor so that id field will initalize
+        public Job(string name, Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCoreCompetency) : this()
+        {
+            Name = name;
+            EmployerName = employerName;
+            EmployerLocation = employerLocation;
+            JobType = jobType;
+            JobCoreCompetency = jobCoreCompetency;
+        }
+
         // TODO: Generate Equals() and GetHashCode() methods.
+
+        public override bool Equals(object obj)
+        {
+            //Two Job objects are equal when their id fields match
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
     }
 }
